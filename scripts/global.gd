f5617d798516a65182ac7b4defcbf6147c9d2864
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-06-16 18:29:41 +0200

    Basic top-down controls and camera

---
+++ scripts/global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export_group("Scene Management")
+@export var menu_scene_path : String
+@export var game_scene_path : String
+
+func load_menu_scene():
+	load_scene(menu_scene_path)
+
+func load_game_scene():
+	load_scene(game_scene_path)
+
+func load_scene(path : String):
+	get_tree().change_scene_to_file(path)
