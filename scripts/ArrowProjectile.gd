863deb330219617745edbb19a94c91eba21f0560
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-06-19 17:12:16 +0200

    Add ground spikes and fix arrow launcher

--- scripts/ArrowProjectile.gd
+++ scripts/ArrowProjectile.gd
@@ -12,13 +12,11 @@ func _ready():
 
 
 func _on_body_entered(body):
-	if is_enabled and (body.owner != get_parent_node_3d()):
+	if is_enabled and (body != get_parent()):
 		if body is PlayerCharacter:
 			body.death()
 		
 		is_enabled = false
 		call_deferred("set_contact_monitor", false)
-		
 		await get_tree().create_timer(destroy_timer).timeout
-		
 		queue_free()
