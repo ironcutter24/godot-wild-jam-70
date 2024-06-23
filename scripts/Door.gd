c73f7df3bd56545b6dbfef868a05baf467ffbd8a
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-06-23 16:48:05 +0200

    Add door tween animations

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -3,8 +3,6 @@ extends StaticBody3D
 
 var _is_open : bool = false
 
-@onready var _door_mesh : MeshInstance3D = $MeshInstance3D/DoorMesh
-
 @export var _pressure_plates : Array[PressurePlate]
 @export var _all_required : bool = false
 @export var _stay_open : bool = false
@@ -18,7 +16,8 @@ func _ready():
 func _physics_process(_delta):
 	if not _stay_open or not _is_open:
 		var should_open = _are_all_pressed() if _all_required else _are_any_pressed()
-		_set_open(should_open)
+		if should_open != _is_open:
+			_set_open(should_open)
 
 
 func _are_any_pressed() -> bool:
@@ -37,5 +36,10 @@ func _are_all_pressed() -> bool:
 
 func _set_open(state: bool) -> void:
 	_is_open = state
-	_door_mesh.hide() if _is_open else _door_mesh.show()
 	$CollisionShape3D.disabled = state
+	
+	var tween_left = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_SINE)
+	tween_left.tween_property($Doors/MeshLeft, "rotation_degrees", Vector3(0.0, 80.0 if state else 0.0, 0.0), 1)
+	
+	var tween_right = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_SINE)
+	tween_right.tween_property($Doors/MeshRight, "rotation_degrees", Vector3(-180.0, -80.0 if state else 0.0, 0.0), 1)
