68ea9a715aa2dbae562fecb95986e74bc1a730b0
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-06-23 17:23:43 +0200

    Add pressure plate sounds

--- scripts/PressurePlate.gd
+++ scripts/PressurePlate.gd
@@ -1,6 +1,18 @@
 class_name PressurePlate
 extends Area3D
 
+var _was_pressed = false
+
+@onready var _audio_player = $AudioStreamPlayer
+
 
 func is_pressed() -> bool:
-	return get_overlapping_bodies().size() > 0
+	var _is_pressed = get_overlapping_bodies().size() > 0
+	if _is_pressed != _was_pressed:
+		if _is_pressed:
+			$PressSound.play()
+		else:
+			$ReleaseSound.play()
+	
+	_was_pressed = _is_pressed
+	return _is_pressed
