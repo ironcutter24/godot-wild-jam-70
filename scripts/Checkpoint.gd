1aee6b59940b02a360fbcf0a409d237644a195df
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-06-23 19:17:25 +0200

    Add checkpoint mesh

--- scripts/Checkpoint.gd
+++ scripts/Checkpoint.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 
+@onready var spawn_point = $SpawnPoint
+
+
 func _on_area_3d_body_entered(body):
 	if body is PlayerCharacter:
 		print("Setting checkpoint...")
-		Global.get_player_controller().set_spawn_point(global_position, global_rotation)
+		Global.get_player_controller().set_spawn_point(spawn_point.global_position, spawn_point.global_rotation)
