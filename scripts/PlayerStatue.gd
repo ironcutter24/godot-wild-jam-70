9519d007007023fddcfbdec78357a8586d9afc18
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-06-24 17:30:26 +0200

    Fix broken statue and pressure plate sounds

--- scripts/PlayerStatue.gd
+++ scripts/PlayerStatue.gd
@@ -9,7 +9,7 @@ func interact_right() -> void:
 	print("Destroying statue")
 	
 	var obj = broken_statue.instantiate()
-	get_tree().current_scene.get_parent().add_child(obj)
+	get_tree().current_scene.add_child(obj)
 	obj.global_position = global_position
 	obj.global_rotation = global_rotation
 	
--- scripts/PressurePlate.gd
+++ scripts/PressurePlate.gd
@@ -1,16 +1,28 @@
 class_name PressurePlate
 extends Area3D
 
-var _was_pressed = false
+
+var _is_pressed = false
+
+
+func _ready():
+	body_entered.connect(_body_changed)
+	body_exited.connect(_body_changed)
 
 
 func is_pressed() -> bool:
-	var _is_pressed = get_overlapping_bodies().size() > 0
+	return _is_pressed
+
+
+func _body_changed(_body) -> void:
+	_refresh_is_pressed()
+
+
+func _refresh_is_pressed() -> void:
+	var _was_pressed = _is_pressed
+	_is_pressed = get_overlapping_bodies().size() > 0
 	if _is_pressed != _was_pressed:
 		if _is_pressed:
 			$PressSound.play()
 		else:
 			$ReleaseSound.play()
-	
-	_was_pressed = _is_pressed
-	return _is_pressed
